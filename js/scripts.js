fdeac4d0b01635543aca15d1491c0aae9ad57c94
Damian Juzyk <damian.juzyk@gmail.com>
2017-12-11 21:51:27 +0200

    add slideBack

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,29 @@
-$(document).ready(function(){
-  var carouselList = $("#carousel ul"),
-  counter = 10000 * 5;
+$(function () {
 
-  function sliderNext(){
-    carouselList.animate({marginLeft:-400}, 500, function(){
+  var carouselList = $("#carousel ul");
+
+setInterval(slideBack, 3000);
+
+  function slideNext (){
+    carouselList.animate({'marginLeft':-400}, 500, function() {
+
       var firstItem = carouselList.find("li:first");
       var lastItem = carouselList.find("li:last");
+
       $(lastItem).after($(firstItem));
       carouselList.css({marginLeft:0});
-      counter++;
-      // console.log(counter % 5);
-      renderDots();
     });
-  }
+  };
 
-  function sliderBack () {
-      carouselList.animate({marginRight:"0"}, 400, function(){
-        var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
-        $(firstItem).after($(lastItem));
-        carouselList.css({marginRight:"400"});
-        counter++;
-        // console.log(counter % 5);
-        renderDots();
-      });
-  }
+  function slideBack (){
+    carouselList.animate({'marginLeft':400}, 300, function() {
 
-  function initDots() {
-  	var lisArray = carouselList.find('li').toArray();
-  	var $dots = $('<div class="dots">');
+      var firstItem = carouselList.find("li:first");
+      var lastItem = carouselList.find("li:last");
 
-  	for (var i = 0; i < lisArray.length; i++) {
-  		$dots.append('<button>'+(i+1)+'</button>');
-  	}
-
-    $('#carousel').after($dots);
-  }
-
-  function renderDots() {
-  	var lisArray = $('.dots button').toArray();
-
-  	$(lisArray).removeClass('active');
-  	$(lisArray[counter % 5]).addClass('active');
-  }
-
-  initDots();
-
-
-  var t;
-
-  function resetInterval() {
-  	t = setInterval(sliderNext, 700);
-  }
-  //resetInterval();
-
-  var z;
-  function reverseInterval() {
-    z = setInterval(sliderBack, 700);
-  }
-  //reverseInterval();
-
-  $('#carousel').mouseout(function () {
-  	clearInterval(z);
-  }).mouseover(reverseInterval);
-
-
+      $(lastItem).after($(firstItem));
+      carouselList.css({marginLeft:0});
     });
+  };
+
+});
