34160febfa8db736b40bc4c303c55579fcf0f85f
Damian Juzyk <damian.juzyk@gmail.com>
2017-12-14 19:59:11 +0400

    add slick

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,12 @@
-$(function () {
-
-  var carouselList = $("#carousel ul");
-
-setInterval(slideBack, 3000);
-
-  function slideNext (){
-    carouselList.animate({'marginLeft':-400}, 500, function() {
-
-      var firstItem = carouselList.find("li:first");
-      var lastItem = carouselList.find("li:last");
-
-      $(lastItem).after($(firstItem));
-      carouselList.css({marginLeft:0});
-    });
-  };
-
-  function slideBack (){
-    carouselList.animate({'marginLeft':400}, 300, function() {
-
-      var firstItem = carouselList.find("li:first");
-      var lastItem = carouselList.find("li:last");
-
-      $(lastItem).after($(firstItem));
-      carouselList.css({marginLeft:0});
-    });
-  };
+$(document).ready(function(){
+  $('.multiple-items').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    accessibility: true,
+    arrows: true
+  });
 
 });
